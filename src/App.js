6c1c816deb3ff1df197c064e5f26e20ad7aaecae
Emmanuel <twentekghana@gmail.com>
2020-05-10 08:53:20 +0800

    Refactored

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Pokemon from "./Pokemon";
+
+const App = (props) => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Pokemon />
+    </div>
+  );
+};
+
+export default App;
